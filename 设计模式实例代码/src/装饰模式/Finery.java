f8d9674c93cb79cda0b2a3daea7f526bc35c97a7
gaoshan <takayamachina@gmail.com>
2022-01-26 15:30:33 +0800

    添加装饰模式

--- 设计模式实例代码/src/装饰模式/Finery.java
+++ 设计模式实例代码/src/装饰模式/Finery.java
@@ -2,7 +2,7 @@ package 装饰模式;
 
 public class Finery extends Person{
     protected Person component ;
-//    private Person component ; // 当component访问权限修饰为private的时候，运行代码试试看。嘻嘻
+//    private Person component ; // 当component访问权限修饰为private的时候，运行代码试试看。嘻嘻 儿子怎么可能访问父亲的私有属性！！！
 
     public void Decorate(Person component){
         this.component = component ;
